72f9b4b09746aefe7c338768dced16e8e96d2357
moretti <moretti@moretti.(none)>
2014-04-11 00:09:28 +0300

    asd

---
+++ card.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "card.h"
+
+int attack (card_t *attacker, card_t *defender){
+	attacker -> card_hp -= defender -> card_damage;
+	defender -> card_hp -= attacker -> card_damage;
+	
+	if (defender -> card_hp <= 0 && attacker -> card_hp <= 0){
+		return 0;
+		//both cards are dead
+	}
+	if (defender -> card_hp < 0 && attacker -> card_hp > 0){
+		return 1;
+		//attacker wins
+	}
+	if (defender -> card_hp > 0 && attacker -> card_hp < 0){
+		return 2;
+		//defender wins
+	}
+	if (defender -> card_hp > 0 && attacker -> card_hp > 0){
+		return 3;
+		//both cards are alive
+	}
+}
+
+int main(){
+	card_t attacker = {"Green card",400, 30, 20};
+	card_t defender = {"Yellow card", 30, 40, 20};
+	 
+	
+	if (attack(&attacker, &defender) == 0){ 
+		printf("Both cards are dead");
+	}
+	if (attack(&attacker, &defender) == 1){ 
+		printf("Attacker won");
+	}
+	if (attack(&attacker, &defender) == 2){
+		printf("Defender won");
+	}
+	if (attack(&attacker, &defender) == 3){ 
+		printf("Both cards are alive");
+	}
+	
+	return 0;
+}
---
+++ card.h
@@ -0,0 +1,9 @@
+#ifndef CARD_H
+	#define CARD_H
+	typedef struct{
+		char *card_name;
+		int card_damage;
+		int card_hp;
+		int card_mana;
+	}card_t;
+#endif
