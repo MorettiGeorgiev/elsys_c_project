d59f19cf0be4a16de731ca47cf87408358ad75ae
MorettiGeorgiev <moretti.georgiev@gmail.com>
2014-04-15 16:28:05 +0300

    Homework 3

---
+++ manapool.h
@@ -0,0 +1,9 @@
+#ifndef MANAPOOL_H
+    #define	MANAPOOL_H
+    #include "card.h"
+        typedef struct{
+        int max_mana;
+        int current_mana;
+        
+    }manapool_t;
+#endif	/* MANAPOOL_H */
