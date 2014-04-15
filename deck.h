dba765de2b0bb0d3905349a74c6d7155aca54676
MorettiGeorgiev <moretti.georgiev@gmail.com>
2014-04-15 16:55:11 +0300

    Update deck.c

--- deck.h
+++ deck.h
@@ -1,10 +1,18 @@
 #ifndef DECK_H
-    #define	DECK_H
-    #define DECK_SIZE 30
-        #include "card.h"    
-            struct deck_t{
-            int top;
-            card_t deck [DECK_SIZE];
+#define	DECK_H
+#define DECK_SIZE 30
+#include "card.h"    
+
+struct hand_t {
+    char *hand_card_name;
+    int hand_card_damage;
+    int hand_card_hp;
+    int hand_card_mana;
+};
+
+struct deck_t {
+    int top;
+    card_t deck [DECK_SIZE];
 };
 void push_card(card_t card_to_push, struct deck_t *player_deck);
 void draw_card(struct deck_t *player_deck, struct hand_t *hand);
